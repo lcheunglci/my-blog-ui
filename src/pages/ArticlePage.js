bd3306456ecdea7b9f4a863b037b0797e3bf1733
Lawrence LCI <lcheunglci@gmail.com>
2022-02-13 14:31:30 -0800

    Add upvote section component to invoke the api

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 import articles from './article-content'
 import ArticlesList from '../components/ArticlesList';
 import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
 import NotFoundPage from './NotFoundPage';
 
 
@@ -27,7 +28,7 @@ const ArticlePage = ( {match}) => {
     return (
         <>
         <h1>{article.title}</h1>
-        <p>This post has been upvoted {articleInfo.upvotes} times.</p>
+        <UpvotesSection upvotes={articleInfo.upvotes} articleName={name} setArticleInfo={setArticleInfo} />
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
         ))}
