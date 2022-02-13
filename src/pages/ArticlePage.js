31abed4f3154f06690e156ea8a2dabd3e9262f62
Lawrence LCI <lcheunglci@gmail.com>
2022-02-13 14:07:25 -0800

    Add fetch to upvotes

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,5 @@
 import 'whatwg-fetch';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import articles from './article-content'
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
@@ -9,12 +9,24 @@ const ArticlePage = ( {match}) => {
     const name = match.params.name;
     const article = articles.find(article => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: []});
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
+
     if (!article) return <NotFoundPage />
     const otherArticles = articles.filter(article => article.name !== name)
 
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This post has been upvoted {articleInfo.upvotes} times.</p>
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
         ))}
