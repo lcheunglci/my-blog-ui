a2dec4476f5fcd2dd9144382214606fa22cfa8d4
Lawrence LCI <lcheunglci@gmail.com>
2022-02-12 22:02:27 -0800

    Add 404 page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import articles from './article-content'
 import ArticlesList from '../components/ArticlesList';
+import NotFoundPage from './NotFoundPage';
+
 
 const ArticlePage = ( {match}) => {
     const name = match.params.name;
     const article = articles.find(article => article.name === name);
 
-    if (!article) return <h1>Article does not exist!</h1>
+    if (!article) return <NotFoundPage />
     const otherArticles = articles.filter(article => article.name !== name)
 
     return (
