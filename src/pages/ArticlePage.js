708b8484fbe63cd4b0179658bbe1a8a370bbffa9
Lawrence LCI <lcheunglci@gmail.com>
2022-02-12 21:54:13 -0800

    Refactor ArticleList into component and reuse it in article page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import articles from './article-content'
+import ArticlesList from '../components/ArticlesList';
 
 const ArticlePage = ( {match}) => {
     const name = match.params.name;
     const article = articles.find(article => article.name === name);
 
     if (!article) return <h1>Article does not exist!</h1>
+    const otherArticles = articles.filter(article => article.name !== name)
 
     return (
         <>
@@ -13,6 +15,10 @@ const ArticlePage = ( {match}) => {
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
         ))}
+
+        <h3>Other articles</h3>
+        <ArticlesList articles={otherArticles} />
+
         </>
     );
 }
